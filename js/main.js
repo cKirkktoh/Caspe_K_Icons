871e9311a38b972d3879a956df5e559ade42fa66
Caspe <k_caspe185622@ATS-HR420-DM23.fc.ca>
2023-01-13 11:17:03 -0500

    added images and js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
 console.log('Javascript is running!');
+
+/* Longer js block comment
+- const is a type of variable - abit of memory that can hold a value
+
+JS uses (can use) AMY valid CSS selector to make a connection to ANY element on the page - it works the same way as CSS selectors do,
+because it's USING css selectors tp find matching elementt(s)
+
+The document is the DOM - the web page and all of its elements
+
+querySelector is the method (function) that makes the connection between JS and te DOM (the elements)
+*/
+
+// step 1 - make the connection t the element(s) you want to interact with
+const jpgGraphic = document.querySelector ("#bitmap");
+
+function logID() {
+    console.log('clicked on this element:', this.id)
+}
+
+// step 2 - decide how you want the user to interact with the object you created in step 1
+jpgGraphic.addEventListener('click', logID);
+
+const svgGraphic = document.querySelector ("#vector");
+
+function logID() {
+    console.log('clicked on this element:', this.id)
+}
+
+svgGraphic.addEventListener('click', logID)
